9660b47e1e5d99120827d362f52d4f6e1db1b7a5
Slyman <74240123+slyman52@users.noreply.github.com>
2022-04-24 23:41:58 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+
+    <title> Bus Reservation System | Home</title>
+    <h1 align="center"> BUS RESERVATION SYSTEM </h1>
+    
+    <link rel="stylesheet" href="home.css">
+
+</head>
+
+<body>
+      <div class="topnav"  class="container", style="border:15px solid green;margin-left:auto;margin-right:auto;">
+        <a href="login.php">Login</a>
+        <a class="active" href="home.php">Homepage</a>
+        <a href="route1.php">Route</a>
+        <a href="seat.php">Book Now</a>
+        <a href="payment.php">Payment</a>
+      </div>
+
+<div class="images" align="center">
+      <img src="image1.jpg"  width="500" height="300">
+      <img src="image2.jpg"  width="500" height="300">
+</div>
+
+<div class="php1" align="center">
+<?php
+echo "<h2>Welcome to Bus Reservation System. We offer a good price and quality services to ensure the 
+customer's safe journey.<h2>Thank you for choosing our site.</h2>";
+?> 
+<button>
+<a href="route1.php" class="button">Route</a><br>
+</button>
+
+</div>
+
+</body>
+
+</html>
